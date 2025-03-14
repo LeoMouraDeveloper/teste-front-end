2858271e65cc3c2f404375f764343d94d6b6ba70
Leonardo Moura <leonardomoura.santos25@gmail.com>
2025-03-14 10:59:57 -0300

    Inicio do Projeto para vaga de Estágio Front-End, Criação do Header e Footer.

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "../components/header";
+import Footer from "../components/footer";
+
+
+const HomePage: React.FC = () => {
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+      
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  );
+};
+
+export default HomePage;
